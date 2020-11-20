2c750d7eb3ecb178fd72cb4f1a3770f6e22f9c59
BigMurloc <s21159@pjwstk.edu.pl>
2020-11-20 14:06:50 +0100

    Changed in UserDB.java List<User> to HashMap<String, User>.

--- app/src/main/java/pl/edu/pjwstk/jaz/UserDB.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/UserDB.java
@@ -2,27 +2,29 @@ package pl.edu.pjwstk.jaz;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class UserDB {
-    //todo change to hashMap
-    //todo what to use as a key in hashMap?
     private User admin = new User("admin", "admin");
     private User example = new User("example", "kotek");
     private User moderator = new User("moderator", "1234");
     private HashMap<String, User> users = new HashMap<>();
 
     public UserDB() {
-        this.users.put("0", admin);
-        this.users.put("1", example);
-        this.users.put("2", moderator);
+        this.users.put(admin.getUsername(), admin);
+        this.users.put(example.getUsername(), example);
+        this.users.put(moderator.getUsername(), moderator);
     }
 
     public void addUser(User user){
-        String key = String.valueOf(users.size());
-        users.put(key, user);
+        users.put(user.getUsername(), user);
+        for(Map.Entry<String, User> set : users.entrySet()) {
+            System.out.println(set.getValue().getUsername());
+        }
+        System.out.println(users.size());
     }
 
 }
