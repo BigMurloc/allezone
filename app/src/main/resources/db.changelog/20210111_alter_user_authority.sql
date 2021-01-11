7b27ae97b78650c749c292cc56f9ecc9b397d8b6
BigMurloc <s21159@pjwstk.edu.pl>
2021-01-11 06:26:57 +0100

    Fixed the problem of not deleting users after the tests. The issue was in sql constraints. Foreign key in app_user_authority table had no "delete on cascade" statement.

---
+++ app/src/main/resources/db.changelog/20210111_alter_user_authority.sql
@@ -0,0 +1,6 @@
+alter table app_user_authority drop constraint fkq3y9v2em5ia10xv9e76ya53sf;
+
+alter table app_user_authority
+	add constraint fkq3y9v2em5ia10xv9e76ya53sf
+		foreign key (app_user_id) references app_user
+			on delete cascade;
