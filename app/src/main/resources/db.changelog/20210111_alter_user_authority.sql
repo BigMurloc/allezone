8188ea1bdc19f30c5292356cff68eb5fedcccb28
BigMurloc <s21159@pjwstk.edu.pl>
2021-01-11 06:45:41 +0100

    added 'if exists' statement so constraint is only dropped when it exists (without that the nullpointerException was being thrown)

--- app/src/main/resources/db.changelog/20210111_alter_user_authority.sql
+++ app/src/main/resources/db.changelog/20210111_alter_user_authority.sql
@@ -1,4 +1,4 @@
-alter table app_user_authority drop constraint fkq3y9v2em5ia10xv9e76ya53sf;
+alter table app_user_authority drop constraint if exists fkq3y9v2em5ia10xv9e76ya53sf;
 
 alter table app_user_authority
 	add constraint fkq3y9v2em5ia10xv9e76ya53sf
