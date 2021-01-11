442b253b9ec5cd3b2a37685e0bd6ef1cdbdf0ad5
BigMurloc <s21159@pjwstk.edu.pl>
2021-01-11 06:57:19 +0100

    test

--- app/src/main/resources/db.changelog/20210111_alter_user_authority.sql
+++
@@ -1,6 +0,0 @@
-alter table app_user_authority drop constraint if exists fkq3y9v2em5ia10xv9e76ya53sf;
-
-alter table app_user_authority
-	add constraint fkq3y9v2em5ia10xv9e76ya53sf
-		foreign key (app_user_id) references app_user
-			on delete cascade;
