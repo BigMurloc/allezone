a153cd860a379d9ca1800f4195fdb8fd4b84df37
BigMurloc <s21159@pjwstk.edu.pl>
2021-01-22 14:39:37 +0100

    refactoring

--- app/src/main/java/pl/edu/pjwstk/jaz/controllers/AuctionController.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/controllers/AuctionController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.*;
 import pl.edu.pjwstk.jaz.controllers.requests.AuctionRequest;
 import pl.edu.pjwstk.jaz.database.repositories.AuctionRepository;
 import pl.edu.pjwstk.jaz.database.services.AuctionService;
+import pl.edu.pjwstk.jaz.database.services.CategoryService;
+import pl.edu.pjwstk.jaz.database.services.SectionService;
 import pl.edu.pjwstk.jaz.database.views.AuctionView;
 
 import java.util.List;
@@ -13,10 +15,17 @@ public class AuctionController {
 
     private final AuctionRepository auctionRepository;
     private final AuctionService auctionService;
+    private final CategoryService categoryService;
+    private final SectionService sectionService;
 
-    public AuctionController(AuctionRepository auctionRepository, AuctionService auctionService) {
+    public AuctionController(AuctionRepository auctionRepository,
+                             AuctionService auctionService,
+                             CategoryService categoryService,
+                             SectionService sectionService) {
         this.auctionRepository = auctionRepository;
         this.auctionService = auctionService;
+        this.categoryService = categoryService;
+        this.sectionService = sectionService;
     }
 
     @PostMapping("/auction")
@@ -26,12 +35,12 @@ public class AuctionController {
 
     @PostMapping("/section")
     public void addSection(@RequestBody SectionRequest sectionRequest){
-        auctionService.addSection(sectionRequest);
+        sectionService.addSection(sectionRequest);
     }
 
     @PostMapping("/category")
     public void addCategory(@RequestBody CategoryRequest categoryRequest){
-        auctionService.addCategory(categoryRequest);
+        categoryService.addCategory(categoryRequest);
     }
 
     @GetMapping("/auction")
